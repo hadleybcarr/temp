ea2b735ba3b900282ad1adba58fada8533610af9
hadleybcarr <hadleybcarr@gmail.com>
2026-05-03 16:49:02 -0400

    overwrite

--- run_yolo_mask.sh
+++ run_yolo_mask.sh
@@ -7,6 +7,7 @@
 #SBATCH -t 02:00:00
 #SBATCH -o logs/maskrcnn-%j.out
 #SBATCH -e logs/maskrcnn-%j.err
+#Run: sbatch run_yolo_mask.sh
 
 
 PROJECT=/oscar/data/class/csci1430/students/hbcarr/parking
@@ -27,4 +28,5 @@ echo "Running on $(hostname)"
 python3 mask_yolo_cnn.py \
     --out-dir      "$PROJECT/detection_cache_mask_yolo_rcnn" \
     --cameras-json "$PROJECT/videos_by_camera.json" \
-    --n-samples-per-video 100
+    --n-samples-per-video 100 \
+    --overwrite
